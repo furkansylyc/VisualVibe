92c98a2d5f93800838f1016fc7f7bc481b595e57
Buse Ünal <unallbuseee@gmail.com>
2026-03-04 00:32:07 +0300

    Created ImageAnalyzer

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation("com.google.mlkit:image-labeling:17.0.7")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
